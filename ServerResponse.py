f7ab44cd2f1367a39736f1370b00f82efe44df2b
thomasadoatwit <116655780+thomasadoatwit@users.noreply.github.com>
2024-04-01 19:13:57 -0400

    Update ServerResponse.py

--- ServerResponse.py
+++ ServerResponse.py
@@ -1,52 +1,59 @@
-#Responsible for sending packets to the clients from server
+# Responsible for sending packets to the clients from server
 import socket
+
 
 class ServerResponse():
 
-    def SendToAll(message, players, port, command):
+    # def __init__(self):
+    #     print("int server")
+
+    def SendToAll(self, message, players, port, command):
         print(players)
         for player in players:
-            #loop over every player
+            # loop over every player
             currentIp = player.getIp()
             currentPort = player.getPort()
 
             if not player.isHouse():
-                SendMessage(message, currentIp, currentPort, command)
+                self.SendMessage(message, currentIp, currentPort, command)
 
-
-
-
-    def SendMessage(message, ip, port, command):
+    def SendMessage(self, message, ip, port, command):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        sock.connect((ip, 1235))
+
+        # print(ip)
+
+
+        # ip here is a tuple with the actual ip and port
+        sock.connect((str(ip[0]), 1235))
 
         sock.sendall(bytes(command.encode('utf-8')))
-        print(command)
+
+
+
 
         ACK = str(sock.recv(50).decode())
+        print(str(command))
+
+        # ACK = "ACK"
         print(ACK)
 
         sock.close()
 
-        if ACK == "ACK":
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        match str(ACK):
+            case "ACK":
+                sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
 
-            sock.connect((ip, 1235))
-            print("Sending message")
+                sock.connect((ip, 1235))
+                print("Sending message")
 
-            sock.sendall(bytes(command.encode('utf-8')))
+                sock.sendall(bytes(command.encode('utf-8')))
 
-            response = str(sock.recv(50).decode())
+                response = str(sock.recv(50).decode())
 
-            sock.close()
+                sock.close()
 
-            return response
-        else:
-            sock.close()
-            print("NACK")
-
-
-
-
-
+                return response
+            case _:
+                sock.close()
+                print("NACK")
